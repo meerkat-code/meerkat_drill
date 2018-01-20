36f8bcad3dcc8e93a36daef9663e0f5c9eafe2e5
gulfa <gunnarroe@gmail.com>
2018-01-20 10:52:01 +0200

    Logging format

--- meerkat_drill/__init__.py
+++ meerkat_drill/__init__.py
@@ -1,6 +1,12 @@
 import logging
 
 console = logging.StreamHandler()
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+console.setFormatter(formatter)
 logger = logging.getLogger(__name__)
 logger.addHandler(console)
+
+
 logger.setLevel(logging.INFO)
