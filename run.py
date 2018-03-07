f612217e057c61f48f3b3dcc0fb74e543cce2971
gulfa <gunnarroe@gmail.com>
2018-03-07 13:14:20 +0200

    Only create queue if SQS

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from meerkat_drill.sqs import create_sqs_queue_if_needed
-from meerkat_drill import process_queue, init
+from meerkat_drill import process_queue, init, config
 
 
 def main():
-    create_sqs_queue_if_needed()
+    if config.external_message_queue == "SQS":
+        create_sqs_queue_if_needed()
     init()
     while True:
         process_queue()
